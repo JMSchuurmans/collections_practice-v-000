73f29735ff2235f9d69ac2f2b049a8ae60561b2b
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-22 18:02:44 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,23 +1,14 @@
 def sort_array_asc(array)
   array.sort do |a, b|
-    if a == b
-      0
-    elsif a < b
-      -1
-    elsif a > b
-      1
-    end
+    a <=> b
   end
 end
 
+
 def sort_array_desc(array)
   array.sort do |a,b|
-    if b == a
-      0
-    elsif b < a
-      -1
-    elsif b > a
-      1
-    end
-  end
+
 end
+
+def sort_array_char_count(array)
+  array.sort |a, b|
